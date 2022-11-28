a3db2562abaa254c0c90934c7eb209f2160171ca
Boris Milanovic <103489050+cyber-milanovicb@users.noreply.github.com>
2022-11-28 23:38:16 +0100

    1st sample of how-to handle arith. operations
    #Python #artithmetics #how-to 
    ----------------------------------2022/2023---------------------
    All you need to know (1st basics collection of examples) regarding the arithmatics

---
+++ arithmentics.py
@@ -0,0 +1,18 @@
+'''
+print(10 // 3)  # displays as a whole number
+
+print(10 / 3)  # displays as "float" type of data, which is most accurate in this case (3.3333...5 - remaining 1)
+
+print(10 % 3)  # this operator shows (if any) remaining there are
+
+print(10 ** 3)  # this operator shows that 10 is powered by 3
+'''
+
+x = 10
+x = x + 3  # example of incrementation of 10 by 3
+# x += 3  # example of augmented assignment operator whereby where augmented or enhanced the assignment is operator
+# does the very same thing as x = x + 3
+# MORE EXAMPLES
+x = 10
+
+print(x)
