e8d44f86206e01a0cf17ec7cbe472a794ecb4efe
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-21 09:04:41 +0100

    Version 3.0

---
+++ escapecharacter.py
@@ -0,0 +1,4 @@
+splitString = "This string  has baen \n split in \n several \n lines  "
+print(splitString)
+tappedString = "1\t2\t3\t4\t5\t6"
+print(tappedString)
--- strings.py
+++ strings.py
@@ -1,3 +1,13 @@
 print("Today is a nice day to learn something new")
 print('Python is great and fun')
 print('When coding you can use "quotes"" in string')
+print("Hello" + " world")
+
+greeting = "Hello"
+name_for = input("Please enter your name: ")
+
+#we can add space like I did in the down line if we want to
+print(greeting + ' ' + name_for)
+
+#sampel without space
+print(greeting + name_for)
