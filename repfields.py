a798523342c2572b7ae0f1107b7afbc7b1306f4e
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-21 18:12:34 +0100

    Added files, changed Data Types, added repfields.py and variables_and_types.py

---
+++ repfields.py
@@ -0,0 +1,16 @@
+age = 32
+print("My age is {0} years".format(age))
+
+# example where whe are going to store integer and strigs in to index numbers in memory using {} for number of index
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}".format
+      (31, "Jan", "Mar,", "May", "Jul", "Aug", "Oct", "Dec"))
+
+# same result here
+print("There are {0} days in  Jan, Mar, May, Jul, Aug, Oct, and Dec".format(31))
+
+# !!! Numbers in curly-brackets determine which of the offered values are going to be used.
+
+print("Jan: {2}, Feb: {0}, March: {2}, April: {1}, May: {2}, Jun: {1}, Jul: {2}, Sep: {1}, Oct: {2}, Nov: {1}, Dec: {2}."
+      .format(28, 30, 31))
+
+# Example of how you can use 3 different integer values and add them to your list of values, and then insert in strings
--- variables_and_types.py
+++ variables_and_types.py
@@ -4,6 +4,22 @@ print('When coding you can use "quotes" in string')
 print("Hello" + " world")
 
 greeting = "Hello"
-name_for = input("Please enter your name: ")
+name = "Boris"
 
-print(greeting + " " + name_for)
+print(greeting + " " + name)
+
+age = 32
+print(age)
+
+# will give you string (str) or integer (int) type of variables used
+print(type(greeting))
+print(type(age))
+
+#  string to years so instead of integer we use string
+age_in_words_stings = "2 years"
+
+#this will give error
+print(name + " is " + age + "years old")
+print(age)
+
+#think errors as your friend, Python with give you where the errors are
