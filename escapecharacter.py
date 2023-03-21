827b21848c33e0c8cfc3d4c6cd42a7dce095e25d
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-21 09:23:33 +0100

    Version 4.0

--- escapecharacter.py
+++ escapecharacter.py
@@ -1,4 +1,19 @@
-splitString = "This string  has baen \n split in \n several \n lines  "
+splitString = "This string  has been \n split in \n several \n lines  "
 print(splitString)
 tappedString = "1\t2\t3\t4\t5\t6"
 print(tappedString)
+#take a look at the tapped charachters - 4 characters
+
+print('Pet shop owner said "No, no, \'e\'s uh, ... he\'s is resting".')
+#or
+print("The pet shop owner said \"No. no, 'e's uh,...he's resting \".")
+#tripple quotes
+print("""The pet shopper said "No, no, \
+'e's uh,...boss is resting.""")
+
+#another exampel of split string - but using backslash to combine
+anotherSString = """This string  has been \
+spit over \
+several \
+lines."""
+print(anotherSString)
