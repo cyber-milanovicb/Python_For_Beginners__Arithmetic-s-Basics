8266d681ee1bb24e29920e936180d58a4f300ed6
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-22 12:20:14 +0100

    added file basicarithmetics.py and rounding of atitmetics in python

--- basicarithmetics.py
+++ basicarithmetics.py
@@ -7,3 +7,14 @@ print(a * b)  # 45
 print(a / b)  # 5.0
 print(a // b)  # 5 integer division, number is rounded down minus infinity
 print(a % b)  # 0 module, the "reminder" after integer division
+
+
+print()
+for i in range(1, 4):
+    print(i)
+
+# You cannot do following
+
+""" for i in rage(1, a/b)
+    print(i) """
+
