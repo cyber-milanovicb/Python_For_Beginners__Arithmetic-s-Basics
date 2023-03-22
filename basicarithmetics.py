4d38c3d56c875e11c99c881564ce115e9561b1d4
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-22 12:18:01 +0100

    added file basicarithmetics.py and rounding of atitmetics in python

---
+++ basicarithmetics.py
@@ -0,0 +1,9 @@
+a = 15
+b = 3
+
+print(a + b)  # 18
+print(a - b)  # 12
+print(a * b)  # 45
+print(a / b)  # 5.0
+print(a // b)  # 5 integer division, number is rounded down minus infinity
+print(a % b)  # 0 module, the "reminder" after integer division
