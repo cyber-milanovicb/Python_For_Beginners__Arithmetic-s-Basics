67b4585afebe57016c99c99cb8ca96a1ab60ba90
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-23 00:42:27 +0100

    Phase 3 / or assignment 3 of strings2.py how to usa the indexes on various ways. Adding now another level, way. In next commit.

--- strings2.py
+++ strings2.py
@@ -43,5 +43,13 @@ print(phrase[3 - 14])
 print(phrase[6 - 14])
 print(phrase[8 - 14])
 
+#                    1
+phrase = "Norwegian     Blue"
+#         0123456789 0  1234
 
+print(phrase[0:6]) # = Norweg
+print(phrase[3:5]) # = weg
 
+# now with these - you will get same result with both examples:
+print(phrase[0:9])
+print(phrase[:9])   # assumption at Python that 1st index nmber is 0.
