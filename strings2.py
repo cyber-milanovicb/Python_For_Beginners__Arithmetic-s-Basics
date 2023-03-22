f3efcdce68e337ea3a57e1efea0688240b8658de
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-23 00:30:13 +0100

    Phase 2 / or assignment 2 of strings2.py how to usa the indexes on verious ways. Adding now another level, way. In next commit.

---
+++ strings2.py
@@ -0,0 +1,47 @@
+phrase = "Norwegian Blue"
+#                   1
+#         012345678901234
+
+
+print(phrase)
+
+print(phrase[3])
+print(phrase[4])
+print(phrase[9])
+
+print(phrase[3])
+print(phrase[6])
+print(phrase[8])
+
+print()
+
+print(phrase[3])
+print(phrase[4])
+print(phrase[9])
+print(phrase[3])
+print(phrase[6])
+print(phrase[8])
+
+print()
+print(phrase[-11])
+print(phrase[-10])
+
+print()
+
+print(phrase[-11])
+print(phrase[-8])
+print(phrase[-6])
+
+print()
+
+# This is also way to do it, as there are in total 14 index numbers, so just using those minus total of charachters and:
+print(phrase[3 - 14])
+print(phrase[4 - 14])
+
+print(phrase[9 - 14])
+print(phrase[3 - 14])
+print(phrase[6 - 14])
+print(phrase[8 - 14])
+
+
+
