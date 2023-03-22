35b6a9c87852cde8595c797bd28a0834afd5be6f
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-22 23:30:56 +0100

    Added assignnment.py file, the resuolution of the assignment after getting over the variables, types of variables, indexing - so that with the knowledge of indexing right values we can work in future.

--- assignnment.py
+++ assignnment.py
@@ -1,10 +1,21 @@
-parrot = {0, 1, 2, 3, 4, 5}
-a = 0
-b = 1
-c = 2
-d = 3
-e: 4
-d: 5
-print(a, b, c, d,)
-print(parrot)
+phrase = "Norwegian Blue"
 
+# Assignment is to get by indexing numbers of the variable "phrase": we win or actually
+# w
+# e
+# [space]
+# w
+# i
+# n
+
+"""HERE'S THE RESULT BELOW"""
+
+print(phrase)
+
+print(phrase[3])
+print(phrase[4])
+print(phrase[9])
+
+print(phrase[3])
+print(phrase[6])
+print(phrase[8])
