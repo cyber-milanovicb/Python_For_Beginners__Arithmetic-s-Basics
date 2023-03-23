610c0b15030d68b0ed714207dc70491bfd041997
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-23 02:21:08 +0100

    Excellent example where we splice and ad / join chars that are (IF NOT) in and split then also excellent exaple in printint out int(val) for val in values

--- slicing_indexes_partofINDEXES2.py
+++ slicing_indexes_partofINDEXES2.py
@@ -4,3 +4,10 @@ phrase = "Norwegian Blue"
 
 print(phrase[0:6:2]) #Nre
 print(phrase[0:6:3]) #Nw
+
+number = "9,223;327:036 854,775;807"
+separators = number[1::4]
+print(separators)
+
+values = "".join(char if char not in separators else " " for char in number).split()
+print([int(val) for val in values])
