ef22653eb3a221f3d01c2baeff79fa5f54e183f6
cyber-milanovicb <milanovic.boris90@gmail.com>
2023-03-23 02:08:13 +0100

    Modification on strings2.py and inserting a new py docuent slicing_indexes_partofINDEXES2.py that relies on the previous one - mentioned.

---
+++ slicing_indexes_partofINDEXES2.py
@@ -0,0 +1,6 @@
+#                  1
+#         012345678901234
+phrase = "Norwegian Blue"
+
+print(phrase[0:6:2]) #Nre
+print(phrase[0:6:3]) #Nw
--- strings2.py
+++ strings2.py
@@ -1,86 +1,11 @@
 phrase = "Norwegian Blue"
+
 #                   1
 #         012345678901234
 
 
-print(phrase)
+print(phrase[0:6]) # norweg
+print(phrase[-14:-8])
 
-print(phrase[3])
-print(phrase[4])
-print(phrase[9])
-
-print(phrase[3])
-print(phrase[6])
-print(phrase[8])
-
-print()
-
-print(phrase[3])
-print(phrase[4])
-print(phrase[9])
-print(phrase[3])
-print(phrase[6])
-print(phrase[8])
-
-print()
-print(phrase[-11])
-print(phrase[-10])
-
-print()
-
-print(phrase[-11])
-print(phrase[-8])
-print(phrase[-6])
-
-print()
-
-# This is also way to do it, as there are in total 14 index numbers, so just using those minus total of charachters and:
-print(phrase[3 - 14])
-print(phrase[4 - 14])
-
-print(phrase[9 - 14])
-print(phrase[3 - 14])
-print(phrase[6 - 14])
-print(phrase[8 - 14])
-
-#                    1
-phrase = "Norwegian     Blue"
-#         0123456789 0  1234
-
-print(phrase[0:6]) # = Norweg
-print(phrase[3:5]) # = weg
-
-# now with these - you will get same result with both examples:
-print(phrase[0:9])
-print(phrase[:9])   # assumption at Python that 1st index nmber is 0.
-
-phrase = "Norwegian Blue"
-#                   1
-#         012345678901234
-print(phrase[10:14]) # USE COLUMN don't forget! Now we have word "BLUE" as you see in my commends about indexes...
-
-print(phrase[10:]) # include always start [X:..] and stop values [..:Y] - in tis case we took all 10 characters frm 14
-# so as we can see it's pretty much left with 4 last letters - as our assignment says - to write down the word BLUE
-
-
-print(phrase[6:])
-print(phrase[:6])
-
-print(phrase[:6] + phrase[6:])   # check this out, we just got our orriginal pharse we 1)sleted first 6 chars
-# and then after that add up with from 6 (incluing it) to the rest indexig numbers.
-
-print(phrase[:]) # no start point no stop so all printed out
-
-
-
-alphabeth = "abcdefghijklmnopqrstuwxyz"  #make a word
-#           01234567891234567891234567
-
-print(alphabeth[1:2])
-print(alphabeth[14:15])
-print(alphabeth[17:18])
-print(alphabeth[8:9])
-print(alphabeth[18:19])
-print(alphabeth[1:2] + alphabeth[14:15] + alphabeth[17:18] + alphabeth[8:9] + alphabeth[18:19])
-
-#CHALLANGE WITH ALBHABET AND MY NAME DONE
+print(phrase[-4:-2])     #Bl
+print(phrase[-4:12])     #Bl        !!! Watch which numbers are you putting and are they negative or not!
